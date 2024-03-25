8c8edf76ebddf4204108343369aa28a6002600e1
Võ Phước Thạnh <92651849+vophuocthanh@users.noreply.github.com>
2024-03-25 23:29:21 +0700

    feat(game): add game questions

---
+++ assets/js/matching-game.js
@@ -0,0 +1,56 @@
+const questions = [
+  { question: 'Bạn yêu GDSC không', answer: 'yes' },
+  { question: 'Bạn là tv GDSC?', answer: 'no' },
+  { question: 'Bạn thích đến đây', answer: 'yes' },
+  { question: 'Bạn biết tôi chứ', answer: 'no' },
+  { question: 'Bạn là SV bách khoa', answer: 'yes' },
+  { question: 'Bạn học giỏi', answer: 'no' },
+  { question: 'Bạn nhác làm', answer: 'no' },
+  { question: 'Bạn là số 1', answer: 'no' },
+  { question: 'Bạn có ny chưa', answer: 'no' },
+];
+
+let currentQuestionIndex = 0;
+let gameCompleted = false;
+
+function handleCardClick(card) {
+  if (gameCompleted) {
+    currentQuestionIndex = 0;
+    gameCompleted = false;
+  }
+
+  const questionModal = document.getElementById('questionModal');
+  const questionText = document.getElementById('questionText');
+
+  if (currentQuestionIndex % 2 === 0) {
+    const question = questions[currentQuestionIndex / 2].question;
+    questionText.textContent = question;
+    questionModal.style.display = 'block';
+  }
+
+  currentQuestionIndex++;
+}
+
+function chooseAnswer(answer) {
+  const questionModal = document.getElementById('questionModal');
+
+  const correctAnswer =
+    questions[Math.floor((currentQuestionIndex - 1) / 2)].answer;
+
+  if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
+    alert('Thành công!');
+    questionModal.style.display = 'none';
+    if (currentQuestionIndex >= questions.length * 2) {
+      alert("You've answered all questions!");
+      gameCompleted = true;
+    }
+  } else {
+    alert('Thất bại! Thử lại.');
+    const randomQuestionIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomQuestionIndex].question;
+    const questionText = document.getElementById('questionText');
+    questionText.textContent = randomQuestion;
+
+    questionModal.style.display = 'block';
+  }
+}
