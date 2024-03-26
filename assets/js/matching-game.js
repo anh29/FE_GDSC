41a3e139090084a7b660b071c6d596535e1db4dd
anh29 <anhhna17710110@gmail.com>
2024-03-26 15:16:38 +0700

    show q&a

--- assets/js/matching-game.js
+++ assets/js/matching-game.js
@@ -1,13 +1,13 @@
 const questions = [
-  { question: 'Bạn yêu GDSC không', answer: 'yes' },
-  { question: 'Bạn là tv GDSC?', answer: 'no' },
-  { question: 'Bạn thích đến đây', answer: 'yes' },
-  { question: 'Bạn biết tôi chứ', answer: 'no' },
-  { question: 'Bạn là SV bách khoa', answer: 'yes' },
-  { question: 'Bạn học giỏi', answer: 'no' },
-  { question: 'Bạn nhác làm', answer: 'no' },
-  { question: 'Bạn là số 1', answer: 'no' },
-  { question: 'Bạn có ny chưa', answer: 'no' },
+  { question: "Bạn yêu GDSC không", answer: "yes" },
+  { question: "Bạn là tv GDSC?", answer: "no" },
+  { question: "Bạn thích đến đây", answer: "yes" },
+  { question: "Bạn biết tôi chứ", answer: "no" },
+  { question: "Bạn là SV bách khoa", answer: "yes" },
+  { question: "Bạn học giỏi", answer: "no" },
+  { question: "Bạn nhác làm", answer: "no" },
+  { question: "Bạn là số 1", answer: "no" },
+  { question: "Bạn có ny chưa", answer: "no" },
 ];
 
 let currentQuestionIndex = 0;
@@ -19,38 +19,37 @@ function handleCardClick(card) {
     gameCompleted = false;
   }
 
-  const questionModal = document.getElementById('questionModal');
-  const questionText = document.getElementById('questionText');
+  const questionModal = document.getElementById("questionModal");
+  const questionText = document.getElementById("questionText");
 
   if (currentQuestionIndex % 2 === 0) {
     const question = questions[currentQuestionIndex / 2].question;
     questionText.textContent = question;
-    questionModal.style.display = 'block';
+    questionModal.style.display = "block";
   }
 
   currentQuestionIndex++;
 }
 
 function chooseAnswer(answer) {
-  const questionModal = document.getElementById('questionModal');
+  const questionModal = document.getElementById("questionModal");
 
   const correctAnswer =
     questions[Math.floor((currentQuestionIndex - 1) / 2)].answer;
 
   if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
-    alert('Thành công!');
-    questionModal.style.display = 'none';
+    questionModal.style.display = "none";
     if (currentQuestionIndex >= questions.length * 2) {
       alert("You've answered all questions!");
       gameCompleted = true;
     }
   } else {
-    alert('Thất bại! Thử lại.');
+    alert("Thất bại! Thử lại.");
     const randomQuestionIndex = Math.floor(Math.random() * questions.length);
     const randomQuestion = questions[randomQuestionIndex].question;
-    const questionText = document.getElementById('questionText');
+    const questionText = document.getElementById("questionText");
     questionText.textContent = randomQuestion;
 
-    questionModal.style.display = 'block';
+    questionModal.style.display = "block";
   }
 }
